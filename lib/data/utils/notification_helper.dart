f9b5f0505282f2a79850280440570b36fa41ee4a
achmadsm <achmad.10120701@mahasiswa.unikom.ac.id>
2022-06-09 09:52:07 +0700

    Add notification_helper.dart

---
+++ lib/data/utils/notification_helper.dart
@@ -0,0 +1,81 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:rxdart/rxdart.dart';
+
+// import '../common/navigation.dart';
+import '../model/article.dart';
+
+final selectNotificationSubject = BehaviorSubject<String>();
+
+class NotificationHelper {
+  static NotificationHelper? _instance;
+
+  NotificationHelper._internal() {
+    _instance = this;
+  }
+
+  factory NotificationHelper() => _instance ?? NotificationHelper._internal();
+
+  Future<void> initNotifications(
+      FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
+    var initializationSettingsAndroid =
+        const AndroidInitializationSettings('app_icon');
+
+    var initializationSettingsIOS = const IOSInitializationSettings(
+      requestAlertPermission: false,
+      requestBadgePermission: false,
+      requestSoundPermission: false,
+    );
+
+    var initializationSettings = InitializationSettings(
+        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: (String? payload) async {
+      if (payload != null) {
+        if (kDebugMode) {
+          print('notification payload: ' + payload);
+        }
+      }
+      selectNotificationSubject.add(payload ?? 'empty payload');
+    });
+  }
+
+  Future<void> showNotification(
+      FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin,
+      ArticlesResult articles) async {
+    var _channelId = "1";
+    var _channelName = "channel_01";
+    var _channelDescription = "dicoding news channel";
+
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        _channelId, _channelName,
+        channelDescription: _channelDescription,
+        importance: Importance.max,
+        priority: Priority.high,
+        ticker: 'ticker',
+        styleInformation: const DefaultStyleInformation(true, true));
+
+    var iOSPlatformChannelSpecifics = const IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        android: androidPlatformChannelSpecifics,
+        iOS: iOSPlatformChannelSpecifics);
+
+    var titleNotification = "<b>Headline News</b>";
+    var titleNews = articles.articles[0].title;
+
+    await flutterLocalNotificationsPlugin.show(
+        0, titleNotification, titleNews, platformChannelSpecifics,
+        payload: json.encode(articles.toJson()));
+  }
+
+  void configureSelectNotificationSubject(String route) {
+    selectNotificationSubject.stream.listen((String payload) async {
+      var data = ArticlesResult.fromJson(json.decode(payload));
+      var article = data.articles[0];
+      // Navigation.intentWithData(route, article);
+    });
+  }
+}

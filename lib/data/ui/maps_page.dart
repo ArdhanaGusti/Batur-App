fcefc152f211333812903bbbd85d2ffa00d292f2
achmadsm <achmad.10120701@mahasiswa.unikom.ac.id>
2022-06-10 22:13:06 +0700

    Add maps_page.dart

---
+++ lib/data/ui/maps_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapsPage extends StatefulWidget {
+  const MapsPage({Key? key}) : super(key: key);
+
+  @override
+  State<MapsPage> createState() => _MapsPageState();
+}
+
+class _MapsPageState extends State<MapsPage> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(-6.902283131688371, 107.61874974418033);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Maps Sample App'),
+          backgroundColor: Colors.green[700],
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
+    );
+  }
+}

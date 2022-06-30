4f904e3cfe5c4bdf893c62ccb9144cfc1fbdcc64
AsepRidwan22 <aszeprydzone@gmail.com>
2022-06-30 21:29:49 +0700

    repair news screen

--- news/lib/presentation/screen/news_screen.dart
+++ news/lib/presentation/screen/news_screen.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:core/presentation/components/appbar/custom_sliver_appbar_dashboard.dart';
 import 'package:core/presentation/components/card/custom_news_card.dart';
-import 'package:core/presentation/components/card/custom_favorite_umkm_card.dart';
 import 'package:core/presentation/components/custom_smart_refresh.dart';
 import 'package:core/presentation/screens/error_screen.dart';
 import 'package:flutter/material.dart';
@@ -68,15 +67,6 @@ class _NewsScreenState extends State<NewsScreen> with TickerProviderStateMixin {
       vsync: this,
       length: 2,
     );
-
-    // Must be repair
-    // Change with to fetch data
-    Timer(const Duration(seconds: 3), () {
-      setState(() {
-        // Change state value if data loaded or failed
-        process = ScreenProcessEnum.loaded;
-      });
-    });
   }
 
   @override
@@ -87,21 +77,7 @@ class _NewsScreenState extends State<NewsScreen> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    if (process == ScreenProcessEnum.loading) {
-      return NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return <Widget>[
-            _buildAppBar(),
-          ];
-        },
-        body: Center(
-          child: LoadingAnimationWidget.horizontalRotatingDots(
-            color: Theme.of(context).colorScheme.tertiary,
-            size: 50.0,
-          ),
-        ),
-      );
-    } else if (process == ScreenProcessEnum.failed) {
+    if (process == ScreenProcessEnum.failed) {
       return const ErrorScreen(
         // Text wait localization
         title: "Koneksi Internet",
@@ -173,203 +149,172 @@ class _NewsScreenState extends State<NewsScreen> with TickerProviderStateMixin {
   }
 
   Widget _buildNewsScreen(BuildContext context, Size screenSize) {
-    return NestedScrollView(
-      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-        return <Widget>[
+    return DefaultTabController(
+      length: 2, // Length of tabs
+      initialIndex: 0,
+      child: CustomScrollView(
+        physics: const NeverScrollableScrollPhysics(),
+        slivers: <Widget>[
           _buildAppBar(),
-        ];
-      },
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(
-              left: 20.0,
-              right: 20.0,
-              top: 10.0,
-            ),
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: SizedBox(
-                // height: 30.0,
-                width: 230.0,
-                child: TabBar(
-                  controller: _controller,
-                  indicator: BoxDecoration(
-                    color: Theme.of(context).colorScheme.tertiary,
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  splashBorderRadius: BorderRadius.circular(10.0),
-                  unselectedLabelColor:
-                      Theme.of(context).colorScheme.tertiaryContainer,
-                  tabs: <Widget>[
-                    Container(
-                      padding: const EdgeInsets.all(2.0),
-                      child: Text(
-                        // Wait Localization
-                        "Terkini",
-                        style: bSubtitle3,
-                      ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20.0,
+                vertical: 10.0,
+              ),
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: SizedBox(
+                  height: 30.0,
+                  width: 230.0,
+                  child: TabBar(
+                    indicator: BoxDecoration(
+                      color: Theme.of(context).colorScheme.tertiary,
+                      borderRadius: BorderRadius.circular(10.0),
                     ),
-                    Container(
-                      padding: const EdgeInsets.all(2.0),
-                      child: Text(
-                        // Wait Localization
-                        "News",
-                        style: bSubtitle3,
+                    splashBorderRadius: BorderRadius.circular(10.0),
+                    unselectedLabelColor:
+                        Theme.of(context).colorScheme.tertiaryContainer,
+                    tabs: <Widget>[
+                      Container(
+                        padding: const EdgeInsets.all(2.0),
+                        child: Text(
+                          // Wait Localization
+                          "Terkini",
+                          style: bSubtitle3,
+                        ),
                       ),
-                    )
-                  ],
+                      Container(
+                        padding: const EdgeInsets.all(2.0),
+                        child: Text(
+                          // Wait Localization
+                          "News",
+                          style: bSubtitle3,
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
-          Flexible(
+          SliverFillRemaining(
             child: TabBarView(
               physics: const BouncingScrollPhysics(),
-              controller: _controller,
               children: <Widget>[
                 CustomSmartRefresh(
                   onLoading: _onLoading,
                   onRefresh: _onRefresh,
                   refreshController: _refreshController,
-                  child: Expanded(
-                    child: CustomScrollView(
-                      shrinkWrap: true,
-                      // physics: const BouncingScrollPhysics(),
-                      slivers: <Widget>[
-                        SliverPadding(
-                          padding: const EdgeInsets.only(
-                            top: 20.0,
-                            left: 20.0,
-                            right: 20.0,
-                          ),
-                          sliver: SliverToBoxAdapter(
-                            child: StreamBuilder<QuerySnapshot>(
-                                stream: FirebaseFirestore.instance
-                                    .collection("News")
-                                    .snapshots(),
-                                builder: (context, snapshot) {
-                                  if (!snapshot.hasData) {
-                                    return Center(
-                                      child: LoadingAnimationWidget
-                                          .horizontalRotatingDots(
-                                        color: Theme.of(context)
-                                            .colorScheme
-                                            .tertiary,
-                                        size: 50.0,
-                                      ),
-                                    );
-                                  }
-                                  return ListView.builder(
-                                    shrinkWrap: true,
-                                    itemBuilder:
-                                        (BuildContext context, int index) {
-                                      // Use Data News
-                                      return Padding(
-                                        padding:
-                                            const EdgeInsets.only(bottom: 15.0),
-                                        child: CustomNewsCard(
-                                          img:
-                                              '${snapshot.data!.docs[index]['coverUrl']}',
+                  child: StreamBuilder<QuerySnapshot>(
+                      stream: FirebaseFirestore.instance
+                          .collection("News")
+                          .snapshots(),
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return Center(
+                            child:
+                                LoadingAnimationWidget.horizontalRotatingDots(
+                              color: Theme.of(context).colorScheme.tertiary,
+                              size: 50.0,
+                            ),
+                          );
+                        }
+                        return Padding(
+                          padding: const EdgeInsets.all(0.0),
+                          child: ListView.builder(
+                            physics: const BouncingScrollPhysics(),
+                            shrinkWrap: true,
+                            itemBuilder: (BuildContext context, int index) {
+                              // Use Data News
+                              return Padding(
+                                padding: const EdgeInsets.only(
+                                    bottom: 7.5, right: 20, left: 20, top: 7.5),
+                                child: CustomNewsCard(
+                                  img:
+                                      '${snapshot.data!.docs[index]['coverUrl']}',
+                                  title: snapshot.data!.docs[index]['title'],
+                                  writer: snapshot.data!.docs[index]
+                                      ['username'],
+                                  date: DateFormat("EEEE, d MMMM yyyy", "id_ID")
+                                      .format(DateTime.parse(
+                                          snapshot.data!.docs[index]['date'])),
+                                  onTap: () {
+                                    // To detail News
+                                    Navigator.push(
+                                      context,
+                                      PageTransition(
+                                        curve: Curves.easeOut,
+                                        type: PageTransitionType.bottomToTop,
+                                        child: NewsDetailScreen(
                                           title: snapshot.data!.docs[index]
                                               ['title'],
+                                          konten: snapshot.data!.docs[index]
+                                              ['content'],
+                                          index: snapshot
+                                              .data!.docs[index].reference,
+                                          urlName: snapshot.data!.docs[index]
+                                              ['coverUrl'],
                                           writer: snapshot.data!.docs[index]
                                               ['username'],
-                                          date: DateFormat(
-                                                  "EEEE, d MMMM yyyy", "id_ID")
-                                              .format(DateTime.parse(snapshot
-                                                  .data!.docs[index]['date'])),
-                                          onTap: () {
-                                            // To detail News
-                                            Navigator.push(
-                                              context,
-                                              PageTransition(
-                                                curve: Curves.easeOut,
-                                                type: PageTransitionType
-                                                    .bottomToTop,
-                                                child: NewsDetailScreen(
-                                                  title: snapshot.data!
-                                                      .docs[index]['title'],
-                                                  konten: snapshot.data!
-                                                      .docs[index]['content'],
-                                                  index: snapshot.data!
-                                                      .docs[index].reference,
-                                                  urlName: snapshot.data!
-                                                      .docs[index]['coverUrl'],
-                                                  writer: snapshot.data!
-                                                      .docs[index]['username'],
-                                                  date: snapshot.data!
-                                                      .docs[index]['date'],
-                                                ),
-                                                duration: const Duration(
-                                                    milliseconds: 150),
-                                                reverseDuration: const Duration(
-                                                    milliseconds: 150),
-                                              ),
-                                            );
-                                          },
+                                          date: snapshot.data!.docs[index]
+                                              ['date'],
                                         ),
-                                      );
-                                    },
-                                    itemCount: snapshot.data!.docs.length,
-                                  );
-                                }),
+                                        duration:
+                                            const Duration(milliseconds: 150),
+                                        reverseDuration:
+                                            const Duration(milliseconds: 150),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              );
+                            },
+                            itemCount: snapshot.data!.docs.length,
                           ),
-                        ),
-                      ],
-                    ),
-                  ),
+                        );
+                      }),
                 ),
                 CustomSmartRefresh(
                   refreshController: _refreshController2,
                   onLoading: _onLoading2,
                   onRefresh: _onRefresh2,
-                  child: CustomScrollView(
-                    shrinkWrap: true,
-                    physics: const BouncingScrollPhysics(),
-                    slivers: <Widget>[
-                      SliverPadding(
-                        padding: const EdgeInsets.only(
-                          top: 20.0,
-                          left: 20.0,
-                          right: 20.0,
-                        ),
-                        sliver: SliverList(
-                          delegate: SliverChildBuilderDelegate(
-                            (BuildContext context, int index) {
-                              // Use Data News
-                              return Padding(
-                                padding: const EdgeInsets.only(bottom: 15.0),
-                                child: CustomNewsCard(
-                                  img:
-                                      "https://cdn1-production-images-kly.akamaized.net/lMHji7xE4GI7YHCWAQumKfFm9Ew=/1200x900/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3554482/original/037161700_1630219411-bandung-5319951_1920.jpg",
-                                  title:
-                                      "Prabowo Atau Anies, Siapa Capres yang Paling Kuat?",
-                                  writer: "Udin Saparudin",
-                                  date: "Jumat, 13 Mei 2022",
-                                  onTap: () {
-                                    // To detail News
-                                    // Navigator.push(
-                                    //   context,
-                                    //   PageTransition(
-                                    //     curve: Curves.easeOut,
-                                    //     type: PageTransitionType.bottomToTop,
-                                    //     child: const NewsDetailScreen(),
-                                    //     duration:
-                                    //         const Duration(milliseconds: 150),
-                                    //     reverseDuration:
-                                    //         const Duration(milliseconds: 150),
-                                    //   ),
-                                    // );
-                                  },
-                                ),
-                              );
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 0.0),
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      physics: const BouncingScrollPhysics(),
+                      itemBuilder: (BuildContext context, int index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(
+                              bottom: 7.5, right: 20, left: 20, top: 7.5),
+                          child: CustomNewsCard(
+                            img:
+                                "https://cdn1-production-images-kly.akamaized.net/lMHji7xE4GI7YHCWAQumKfFm9Ew=/1200x900/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3554482/original/037161700_1630219411-bandung-5319951_1920.jpg",
+                            title:
+                                "Prabowo Atau Anies, Siapa Capres yang Paling Kuat?",
+                            writer: "Udin Saparudin",
+                            date: "Jumat, 13 Mei 2022",
+                            onTap: () {
+                              // To detail News
+                              // Navigator.push(
+                              //   context,
+                              //   PageTransition(
+                              //     curve: Curves.easeOut,
+                              //     type: PageTransitionType.bottomToTop,
+                              //     child: const NewsDetailScreen(),
+                              //     duration:
+                              //         const Duration(milliseconds: 150),
+                              //     reverseDuration:
+                              //         const Duration(milliseconds: 150),
+                              //   ),
+                              // );
                             },
-                            childCount: 5,
                           ),
-                        ),
-                      ),
-                    ],
+                        );
+                      },
+                      itemCount: 10,
+                    ),
                   ),
                 ),
               ],

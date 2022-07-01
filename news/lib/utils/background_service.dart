5e425dbe8847e4afd76875f689171cfd8e79e5c8
achmadsm <achmad.10120701@mahasiswa.unikom.ac.id>
2022-07-01 22:48:18 +0700

    Add notification

---
+++ news/lib/utils/background_service.dart
@@ -0,0 +1,41 @@
+import 'dart:isolate';
+import 'dart:ui';
+
+import 'package:flutter/foundation.dart';
+
+import '../data/datasources/news_remote_data_source.dart';
+import 'notification_helper.dart';
+
+final ReceivePort port = ReceivePort();
+
+class BackgroundService {
+  static BackgroundService? _instance;
+  static const String _isolateName = 'isolate';
+  static SendPort? _uiSendPort;
+
+  BackgroundService._internal() {
+    _instance = this;
+  }
+
+  factory BackgroundService() => _instance ?? BackgroundService._internal();
+
+  void initializeIsolate() {
+    IsolateNameServer.registerPortWithName(
+      port.sendPort,
+      _isolateName,
+    );
+  }
+
+  static Future<void> callback() async {
+    if (kDebugMode) {
+      print('Alarm fired!');
+    }
+    final NotificationHelper _notificationHelper = NotificationHelper();
+    var result = await NewsRemoteDataSource().bandungNewsId();
+    // await _notificationHelper.showNotification(
+    //     flutterLocalNotificationsPlugin, result);
+
+    _uiSendPort ??= IsolateNameServer.lookupPortByName(_isolateName);
+    _uiSendPort?.send(null);
+  }
+}

13cc5ad82ff3bbdd30474ee9008febc81ec2e697
achmadsm <achmad.10120701@mahasiswa.unikom.ac.id>
2022-07-01 22:46:05 +0700

    Update Maps

--- tourism/lib/data/datasource/tourism_remote_data_source.dart
+++ tourism/lib/data/datasource/tourism_remote_data_source.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 
 
 class TourismRemoteDataSource {
-  static const apiKey = 'AIzaSyAO1b9CLWFz6Y9NG14g2gpYP7TQWPRsPG0';
+  static const apiKey = 'YOUR KEY HERE';
   static const baseUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?&';
   static const location = '-6.905977%2C107.613144';
   static const radius = 1500;

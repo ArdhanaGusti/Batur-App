1440b80da47ba6a630a295cfa6da5873c743c4a9
ArdhanaGusti <syahputra.ardhana@gmail.com>
2022-07-03 21:34:17 +0700

    s

--- core/lib/utils/config.dart
+++ core/lib/utils/config.dart
@@ -1,7 +1,10 @@
 class Config {
-  final mapsKey = 'YOUR KEY HERE';
-  final newsKey = 'YOUR KEY HERE';
-  final placeUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
-  final placeDetailUrl = 'https://maps.googleapis.com/maps/api/place/details/json?';
-  final photosUrl = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=';
+  final mapsKey = 'AIzaSyAO1b9CLWFz6Y9NG14g2gpYP7TQWPRsPG0';
+  final newsKey = '9a9d9288c8af4e4aa7abf2b7b913247a';
+  final placeUrl =
+      'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
+  final placeDetailUrl =
+      'https://maps.googleapis.com/maps/api/place/details/json?';
+  final photosUrl =
+      'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=';
 }

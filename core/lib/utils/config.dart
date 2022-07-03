c395feaa629a0d83c945a21de2fb1e372a44f02e
achmadsm <achmad.10120701@mahasiswa.unikom.ac.id>
2022-07-03 19:36:38 +0700

    Add config.dart

---
+++ core/lib/utils/config.dart
@@ -0,0 +1,7 @@
+class Config {
+  final mapsKey = 'YOUR KEY HERE';
+  final newsKey = 'YOUR KEY HERE';
+  final placeUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
+  final placeDetailUrl = 'https://maps.googleapis.com/maps/api/place/details/json?';
+  final photosUrl = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=';
+}
